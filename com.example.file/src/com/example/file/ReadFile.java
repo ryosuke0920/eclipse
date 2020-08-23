10a78ee0438393be1b6ce497b7ac78450a4bffdb
Ryosuke Ohta <mailryosuke@gmail.com>
2020-08-23 16:35:02 +0900

    Added a event listener.

--- com.example.file/src/com/example/file/ReadFile.java
+++ com.example.file/src/com/example/file/ReadFile.java
@@ -7,13 +7,15 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class ReadFile {
 	final static Logger logger = Logger.getLogger(ReadFile.class.toString());
+	static Connection conn = null;
 
 	public static void main(String[] args) throws Exception {
 		String driver = args[0];
@@ -33,21 +35,35 @@ public class ReadFile {
 		scan.close();
 		if (!Objects.equals(str, "Y")) {
 			System.out.println("Canceled.");
-			System.exit(0);
+			System.exit(1);
 		}
 
 		Class.forName(driver);
-		Connection conn = null;
 
 		try {
 			conn = DriverManager.getConnection(url, user, password);
 			conn.setAutoCommit(false);
 
-			CsvLoader loader = new CsvLoader();
-			loader.setFileFromString(filepath);
-			loader.setConn(conn);
-			loader.setSql("INSERT INTO sample (name) VALUES (?)");
-			loader.readAndInsert();
+			CsvReader reader = new CsvReader();
+			reader.setFileFromString(filepath);
+			reader.registerLineReadListener(new LineReadListener() {
+				@Override
+				public void onLineRead(String line) {
+					logger.info("onLinedRead: " + line);
+					try {
+						PreparedStatement ps = conn.prepareStatement("INSERT INTO sample (name) VALUES (?)");
+						ps.setString(1, line);
+						int i = ps.executeUpdate();
+						logger.info(i + " rows affected.");
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			reader.registerLineReadListener(line -> {
+				logger.info("test call...");
+			});
+			reader.read();
 
 			conn.commit();
 
@@ -62,42 +78,48 @@ public class ReadFile {
 	}
 }
 
-class CsvLoader {
-	final static Logger logger = Logger.getLogger(CsvLoader.class.toString());
+interface LineReadListener {
+	public void onLineRead(String line);
 
+}
+
+class CsvReader {
+	final static Logger logger = Logger.getLogger(CsvReader.class.toString());
 	String encoding = "Windows-31J";
 	String crlf = "\n";
+
 	File file;
-	Connection conn;
-	String sql;
+
+	private List<LineReadListener> listeners = new ArrayList<>();
 
 	public void setFileFromString(String str) {
 		setFile(new File(str));
 	}
 
-	public void readAndInsert() {
+	public void read() {
 		BufferedReader reader = null;
 		try {
 			logger.info("Reading file is " + file.toString());
 			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
-				loadedLine(line);
+				notifyLineReadListener(convertLine(line));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void loadedLine(String line) throws SQLException {
-		line = line.replace(crlf, "");
-		logger.info("A line is " + line);
+	public String convertLine(String line) {
+		return line.replace(crlf, "");
+	}
 
-		/* イベントハンドラにしたい */
-		PreparedStatement ps = conn.prepareStatement(sql);
-		ps.setString(1, line);
-		int i = ps.executeUpdate();
-		logger.info(i + " rows affected.");
+	public void registerLineReadListener(LineReadListener listener) {
+		this.listeners.add(listener);
+	}
+
+	protected void notifyLineReadListener(String line) {
+		this.listeners.forEach(listener -> listener.onLineRead(line));
 	}
 
 	public File getFile() {
@@ -106,21 +128,5 @@ class CsvLoader {
 
 	public void setFile(File file) {
 		this.file = file;
-	}
-
-	public Connection getConn() {
-		return conn;
-	}
-
-	public void setConn(Connection conn) {
-		this.conn = conn;
-	}
-
-	public String getSql() {
-		return sql;
-	}
-
-	public void setSql(String sql) {
-		this.sql = sql;
 	}
 }
